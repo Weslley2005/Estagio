837156a894a7e79c7edf593795ce2eacd05caf9e
Weslley2005 <weslleytortelli@outlook.com>
2024-07-29 13:13:48 -0400

    Verificação ponto funcionando

--- index.js
+++ index.js
@@ -1,29 +1,93 @@
 const express = require('express');
-
+const cron = require('node-cron');
+const nodemailer = require('nodemailer');
 require('dotenv').config();
 const { login } = require('./src/acessoPonto');
+const { verificarPonto } = require('./src/verificaPonto');
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 3000;
 const HOST = '0.0.0.0';
 
 const app = express();
 
 app.get('/', (req, res) => {
     res.send('Hello World!');
+    console.log('Rota / foi acessada');
 });
-  const url = process.env.URL;
-  const empresa = process.env.EMPRESA;
-  const usuario = process.env.USUARIO;
-  const senha = process.env.SENHA;  
 
-login(url, empresa, usuario, senha).then(success => {
-  if (success) {
-    console.log('Login bem-sucedido!');
-  } else {
-    console.log('Falha no login.');
-  }
+const url = process.env.URL;
+const empresa = process.env.EMPRESA;
+const usuario = process.env.USUARIO;
+const senha = process.env.SENHA;
+const emailDe = process.env.EMAIL_DE;
+const emailPara = process.env.EMAIL_PARA.split(',');
+const emailSenha = process.env.EMAIL_SENHA;
+
+const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+        user: emailDe,
+        pass: emailSenha
+    }
+});
+console.log('Transportador de e-mail configurado');
+
+function enviarEmail(funcionariosSemPonto) {
+    console.log('Preparando para enviar e-mail');
+    const mailOptions = {
+        from: emailDe,
+        to: emailPara.join(','),
+        subject: 'Funcionários que não bateram o ponto',
+        text: `Os seguintes funcionários não bateram o ponto: ${funcionariosSemPonto.join(', ')}`
+    };
+
+    transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+            console.log('Erro ao enviar e-mail:', error);
+            console.error('Detalhes do erro:', error);
+            return;
+        }
+        console.log('E-mail enviado:', info.response);
+    });
+}
+
+async function executarVerificacaoDePonto(pontoIndex) {
+    console.log(`Iniciando processo de verificação de ponto para pontos_${pontoIndex}...`);
+
+    try {
+        const success = await login(url, empresa, usuario, senha);
+        if (success) {
+            console.log('Login bem-sucedido! Verificando ponto...');
+            const funcionariosSemPonto = await verificarPonto(pontoIndex);
+            if (funcionariosSemPonto && funcionariosSemPonto.length > 0) {
+                console.log(`Funcionários que não bateram o ponto: ${funcionariosSemPonto.join(', ')}`);
+                enviarEmail(funcionariosSemPonto);
+            } else {
+                console.log('Todos os funcionários bateram o ponto.');
+            }
+        } else {
+            console.log('Falha no login.');
+        }
+    } catch (err) {
+        console.error('Erro ao tentar realizar login:', err);
+    }
+}
+
+const horarios = [
+    { horario: '10 07 * * *', pontoIndex: 0 },
+    { horario: '10 11 * * *', pontoIndex: 1 },
+    { horario: '10 13 * * *', pontoIndex: 2 },
+    { horario: '10 17 * * *', pontoIndex: 3 },
+];
+
+horarios.forEach(({ horario, pontoIndex }) => {
+    console.log(`Agendando tarefa para o horário: ${horario}`);
+    cron.schedule(horario, () => executarVerificacaoDePonto(pontoIndex), {
+        scheduled: true,
+        timezone: "America/Campo_Grande"
+    });
 });
 
 app.listen(PORT, HOST, () => {
-    console.log(`Server Rodando`);
+    console.log(`Server rodando em http://${HOST}:${PORT}`);
 });
--- src/acessoPonto.js
+++ src/acessoPonto.js
@@ -1,4 +1,5 @@
 const puppeteer = require('puppeteer');
+const cheerio = require('cheerio');
 const fs = require('fs');
 require('dotenv').config();
 
@@ -15,9 +16,11 @@ async function login(url, empresa, usuario, senha) {
 
     console.log('Preenchendo formulário de login...');
     await page.type('#loginEmpresa', empresa);
+    console.log('Empresa preenchida:', empresa);
     await page.type('#loginUsuario', usuario);
+    console.log('Usuário preenchido:', usuario);
     await page.type('#loginSenha', senha);
-    console.log('Formulário preenchido.');
+    console.log('Senha preenchida.');
 
     console.log('Enviando formulário de login...');
     await page.click('#btnSubmit');
@@ -26,7 +29,7 @@ async function login(url, empresa, usuario, senha) {
     console.log('Aguardando redirecionamento após o login...');
     await page.waitForNavigation({ waitUntil: 'networkidle2', timeout: 100000 });
     console.log('Redirecionamento concluído.');
-    
+
     console.log('Verificando sucesso do login...');
     const success = await page.evaluate(() => {
       return !!document.querySelector('.perfect-scrollbar-on');
@@ -35,33 +38,80 @@ async function login(url, empresa, usuario, senha) {
     if (!success) {
       throw new Error('Login não foi bem-sucedido');
     }
+    console.log('Login bem-sucedido.');
 
+    await page.reload();
+  
     console.log('Acessando URL de ponto...');
     if (!process.env.URLPONTO) {
       throw new Error('URLPONTO não está definida nas variáveis de ambiente');
     }
     await page.goto(process.env.URLPONTO, { waitUntil: 'networkidle2' });
+    console.log('URL de ponto acessada.');
+
+    await page.reload();
 
     console.log('Selecionando campo de data e inserindo a data de hoje...');
     const today = new Date();
-    const formattedDate = (`0${today.getDate()}`).slice(-2) + '/' + 
-                          (`0${today.getMonth() + 1}`).slice(-2) + '/' + 
+    const formattedDate = (`0${today.getDate()}`).slice(-2) + '/' +
+                          (`0${today.getMonth() + 1}`).slice(-2) + '/' +
                           today.getFullYear(); 
     await page.evaluate((date) => {
       document.querySelector('.datepicker').value = date;
     }, formattedDate);
-    console.log('Data de hoje inserida.');  
-    
+    console.log('Data de hoje inserida:', formattedDate);
+
     console.log('Clicando no botão Exibir...');
     await page.click('#btnExibir');
     console.log('Botão Exibir clicado.');
 
-    console.log('Aguardando resposta da API...');
-    const usersResponse = await page.waitForResponse(
-      response => response.url() === 'https://client.ezpointweb.com.br/pontoDiaSearch.readListaFiltrada.do' && response.status() === 200
-    );
-    const users = await usersResponse.json();
-    console.log('Resposta da API recebida:', users);
+    console.log('Configurando interceptação de requisições XHR para o link desejado...');
+    await page.setRequestInterception(true);
+
+    page.on('request', request => {
+      if (request.url().includes(process.env.URLAPI)) {
+        console.log('Requisição interceptada:', request.url());
+        request.continue();
+      } else {
+        request.continue();
+      }
+    });
+
+    let responseProcessed = false;
+    let dadosInput;
+
+    page.on('response', async response => {
+      const url = response.url();
+      if (url.includes(process.env.URLAPI)) {
+        const text = await response.text();
+        const $ = cheerio.load(text);
+        
+        dadosInput = $('#dadosTabela').val();
+        console.log('Valor do input hidden:', dadosInput);
+        responseProcessed = true;
+      }
+    });
+
+    await new Promise((resolve, reject) => {
+      const checkResponse = setInterval(() => {
+        if (responseProcessed) {
+          clearInterval(checkResponse);
+          resolve();
+        }
+      }, 1000);
+      
+      setTimeout(() => {
+        clearInterval(checkResponse);
+        reject(new Error('Timeout ao aguardar a resposta da requisição'));
+      }, 30000);
+    });
+
+    if (dadosInput) {
+      console.log('Salvando dados no arquivo Dados.json...');
+      const dados = JSON.parse(dadosInput);
+      fs.writeFileSync('Dados.json', JSON.stringify(dados, null, 2));
+      console.log('Dados salvos com sucesso.');
+    }
 
     console.log('Fechando navegador...');
     await browser.close();
